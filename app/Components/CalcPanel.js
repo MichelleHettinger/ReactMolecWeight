ef6ff0fb67cb7d5c03689da74aa8a747fb868544
Michelle Hettinger <Michelle Hettinger>
2016-12-20 03:41:08 -0500

    commented out firebase stuff in the meantime

--- app/Components/CalcPanel.js
+++ app/Components/CalcPanel.js
@@ -38,24 +38,24 @@ export default class CalcPanel extends Component {
 		//Till next time
 
 
-		const user = firebase.auth().currentUser;
+		// const user = firebase.auth().currentUser;
 
-		if (user!=null){
-			console.log(user);
+		// if (user!=null){
+		// 	console.log(user);
 
-			firebase.database().ref('users/' + user.uid).set({
-				chemicalName: this.state.chemicalName,
-				elements: this.props.mainState.elements,
-				multipliers: this.props.mainState.multipliers,
-				total: this.props.mainState.total,
-				parenMultiplier: this.props.mainState.parenMultiplier,
-			}, () => {
-				(console.log('Wrote to database'))
-			});
-		}
-		else {
-			alert('Login First')
-		}
+		// 	firebase.database().ref('users/' + user.uid).set({
+		// 		chemicalName: this.state.chemicalName,
+		// 		elements: this.props.mainState.elements,
+		// 		multipliers: this.props.mainState.multipliers,
+		// 		total: this.props.mainState.total,
+		// 		parenMultiplier: this.props.mainState.parenMultiplier,
+		// 	}, () => {
+		// 		(console.log('Wrote to database'))
+		// 	});
+		// }
+		// else {
+		// 	alert('Login First')
+		// }
 
 	}
 
